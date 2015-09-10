53f62866bdbac3a50742a6fc8ec3c8dcddb662bb
willgrz <william@william.si>
2015-09-10 06:05:05 +0200

    moved backup hold time to variable in serverlist
    major change - can/MUST now be defined by server list entry

--- backup.sh
+++ backup.sh
@@ -1,22 +1,26 @@
-#!/usr/local/bin/bash
+#!/bin/bash
+#!/usr/local/bin/bash on BSD
 
 #backup root dir
 backupdir=/data/serverbackups/autobackup
 #file with servers
 serverfile=serverlist
-#how long to keep backups, 0 to disable
-holdbackup=30
 #inetnal network
 intnet="10.250."
 #gpg key for encryption
 gkey=2AC179D0
+#gpg binary, change for BSD
+cgpg=$(which gpg)
+
+
 
 
 function backupbox {
-#call backupbox servername ip:port user"
+#call backupbox servername ip:port user holdb"
 		sname=$1
 		sipp=$2
 		suser=$3
+		sholdb=$4
 		#convert IP:Port to IP and Port
                 eip=$(echo $sipp | sed -e 's/:/ /g' | awk '{print $1}')
                 eport=$(echo $sipp | sed -e 's/:/ /g' | awk '{print $2}')
@@ -26,22 +30,23 @@ function backupbox {
 		#mkdir backupserver dir if not already available
 		mkdir $backupserver
 		#remove old files
-		if [ "$holdbackup" != "0" ]; then
-			find $backupdir/$backupserver/*.enc -mtime +$holdbackup -exec rm {} \;
+		if [ "$sholdb" != "0" ]; then
+			find $backupdir/$backupserver/*.enc -mtime +$sholdb -exec rm {} \;
 		fi
 		#set ymd
 		ymdstr=$(date +'%Y-%m-%d-%H%M')
 		#run actual backup
-		ssh -p $eport $suser@$eip 'tar --numeric-owner --exclude "/var/vmail" --exclude "/nfs" --exclude "/kvm" --exclude "/data" --exclude "/var/www/html" --exclude "/mnt" --exclude "/var/spool/squid" --exclude "/var/spool/squid3" --exclude "/proc" --exclude "/backup" --exclude "/sys" --exclude "/dev" -cz /' | /usr/local/bin/gpg --trust-model always --encrypt --recipient $gkey -o "$backupserver/$ymdstr.tar.gz.enc"
+		ssh -p $eport $suser@$eip 'tar --numeric-owner --exclude "/smokeping" --exclude "/var/vmail" --exclude "/nfs" --exclude "/kvm" --exclude "/data" --exclude "/var/www/html" --exclude "/mnt" --exclude "/var/spool/squid" --exclude "/var/spool/squid3" --exclude "/proc" --exclude "/backup" --exclude "/sys" --exclude "/dev" -cz /' | $cgpg --trust-model always --encrypt --recipient $gkey -o "$backupserver/$ymdstr.tar.gz.enc"
 }
 
 
-for server in $(cat serverlist | awk '{print $1}'); do
+for server in $(cat serverlist | awk '{print $1}' | grep -v '#'); do
 	backupserver=$server
 	inthostp=$(cat serverlist | grep $server | awk '{print $2}')
         exthostp=$(cat serverlist | grep $server | awk '{print $3}')
         luser=$(cat serverlist | grep $server | awk '{print $4}')
-	echo "backing up $backupserver with int $inthostp and ext $exthostp and user $luser"
+	holdb=$(cat serverlist | grep $server | awk '{print $5}')
+	echo "backing up $backupserver with int $inthostp and ext $exthostp and user $luser with holdbackup of $holdb"
 
 #check if backup server has int
 intcheck=dummy
@@ -57,16 +62,16 @@ intcheck=dummy
 #take that and put it in if-else
 	if [ "$intcheck" == "0" ]; then
 #		echo "has no int, run ext, no further check"
-		backupbox $backupserver $exthostp $luser
+		backupbox $backupserver $exthostp $luser $holdb
 
 	elif [ "$intcheck" == "1" ]; then
 #		echo 'has int, test int'
 		if [ "$inthostp" != "0:0" ]; then
 			#server has int, run int
-			backupbox $backupserver $inthostp $luser
+			backupbox $backupserver $inthostp $luser $holdb
 		elif [ "$inthostp" == "0:0" ]; then
 			#server has no int, run ext
-			backupbox $backupserver $exthostp $luser
+			backupbox $backupserver $exthostp $luser $holdb
 		fi
 	else
 		echo "Error - intcheck not working"
